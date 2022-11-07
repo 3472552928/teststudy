5b88d26abd4c1300c711d7a4bf1a76bdc6b6bf84
3472552928 <110899482+3472552928@users.noreply.github.com>
2022-11-07 11:57:56 +0800

    修改了测试报告

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 if __name__ == '__main__':
     # 1、查找所有测试报告用例并执行
     pytest.main(["-s",
-                 "--alluredir","report/allure_report","--clean-alluredir"])
+                 "--alluredir=report/allure_report","--clean-alluredir"])
     # 2、把测试报告生成html（第一种方式）  serve启动
     # os.system(r"allure serve ./allure_raw")
     # 3、把测试报告生成html   generate生成报告    clean删除报告

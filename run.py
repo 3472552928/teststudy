498269dedb2f7a84e58b6ecdf87101c91c2fee07
3472552928 <110899482+3472552928@users.noreply.github.com>
2022-11-07 09:58:21 +0800

    修改了测试报告

--- run.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import os
-
-# 运行测试报告
-if __name__ == '__main__':
-    pytest.main(["-sq",
-                 "-alluredir","./allure_resulit"])
-    os.system(r"allure serve ./allure_resulIt")

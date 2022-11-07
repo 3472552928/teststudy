d70b9c3d784c56799180f53a00d03a4f3df53d0c
3472552928 <110899482+3472552928@users.noreply.github.com>
2022-11-07 11:21:47 +0800

    修改了测试报告

--- report/__init__.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-# @time :
-# @author : 段璐威
-# @email : duanluwei925@163.com
-# @file : .py
-# @software: pycharm
--- run.py
+++ run.py
@@ -3,6 +3,11 @@ import os
 
 # 运行测试报告
 if __name__ == '__main__':
-    pytest.main(["-sq",
-                 "--alluredir","./allure_raw"])
-    os.system(r"allure serve ./allure_raw")
+    # 1、查找所有测试报告用例并执行
+    pytest.main(["-s",
+                 "--alluredir","report/allure_report","--clean-alluredir"])
+    # 2、把测试报告生成html（第一种方式）  serve启动
+    # os.system(r"allure serve ./allure_raw")
+    # 3、把测试报告生成html   generate生成报告    clean删除报告
+    os.system("allure generate report/allure_report -o report/report_html --clean")
+
